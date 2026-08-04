c8b4ba4479cbc2c726fe5ad7a3d78c9a61137726
Mahmud Pias <mahmudpias9@gmail.com>
2026-08-04 22:36:32 +0600

    week 4 added

---
+++ Module 13/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.dxaldkrpfzphciqgnrmc:testpias321uj@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
